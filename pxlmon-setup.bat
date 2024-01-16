9b6b0cf3c356451d15d0fe5519d42d3d1de90ce9
Connor <cdearden9115@protonmail.com>
2024-01-17 00:31:38 +1000

    Add files via upload

--- pxlmon-setup.bat
+++ pxlmon-setup.bat
@@ -1,38 +1,80 @@
 @echo off
-set version=2.0
+set version=2.1
 set dirname=Picklemon%version%
-cd %AppData%\
-mkdir %dirname%\mods\
-:: Copy required files from main MC installation
-echo Copying settings and keybinds
-xcopy /q .minecraft\launcher_accounts_microsoft_store.json %dirname%\ >nul
-xcopy /q .minecraft\launcher_profiles.json %dirname%\ >nul
-xcopy /q .minecraft\launcher_settings.json %dirname%\ >nul
-xcopy /q .minecraft\options.txt %dirname%\ >nul
-xcopy /q .minecraft\servers.dat %dirname%\ >nul
-xcopy /q .minecraft\usercache.json %dirname%\ >nul
-if exist .minecraft\optionsof.txt (
-	xcopy /q .minecraft\optionsof.txt %dirname%\ >nul
+if not exist picklemon.zip (
+	echo picklemon.zip is not in the same folder!
+	echo Make sure the zip is in the following folder:
+	echo %~dp0
+	pause
+	EXIT /B 1
 )
+echo|set /p="Java? "
+set jreInstalled=true
+powershell "Get-Package java*" | find "Exception" >nul 2>&1 && set jreInstalled=false
+if %jreInstalled%==false (
+	echo NOT installed! Installing...
+	winget install Oracle.JavaRuntimeEnvironment >nul 2>&1
+) else (
+	echo Installed
+)
+cd %AppData%\
+echo|set /p="Forge? "
+if NOT EXIST .minecraft\versions\1.16.5-forge-36.2.34 (
+	echo NOT installed! Installing...
+	powershell Invoke-WebRequest -Uri https://maven.minecraftforge.net/net/minecraftforge/forge/1.16.5-36.2.34/forge-1.16.5-36.2.34-installer.jar -OutFile $env:USERPROFILE\Downloads\forge-1.16.5-36.2.34-installer.jar | echo >nul 2>&1
+	java -jar %USERPROFILE%\Downloads\forge-1.16.5-36.2.34-installer.jar >nul 2>&1
+	ping 127.0.0.1 -n 1 -w 500 >nul
+	del forge-1.16.5-36.2.34-installer.jar.log
+	del %USERPROFILE%\Downloads\forge-1.16.5-36.2.34-installer.jar
+) else (
+	echo Installed
+)
+if exist %dirname%\ (
+	echo Picklemon already installed!
+	pause
+	EXIT /B 1
+)
+mkdir %dirname%\mods\
 echo Extracting mods to installation folder
 powershell Expand-Archive -Path %~dp0\picklemon.zip -DestinationPath %dirname%\mods\
-:: Get date so the installation is selected by default
-FOR /F "tokens=* USEBACKQ" %%F IN (`echo %DATE%`) DO (
-	SET timedate=%%F
+
+if EXIST .minecraft\ (
+	setlocal enabledelayedexpansion
+	:: Copy required files from main MC installation
+	echo Copying settings and keybinds
+	xcopy /q .minecraft\launcher_accounts_microsoft_store.json %dirname%\ >nul 2>&1
+	xcopy /q .minecraft\launcher_profiles.json %dirname%\ >nul 2>&1
+	xcopy /q .minecraft\launcher_settings.json %dirname%\ >nul 2>&1
+	xcopy /q .minecraft\options.txt %dirname%\ >nul 2>&1
+	xcopy /q .minecraft\servers.dat %dirname%\ >nul 2>&1
+	xcopy /q .minecraft\usercache.json %dirname%\ >nul 2>&1
+	if exist .minecraft\optionsof.txt (
+		xcopy /q .minecraft\optionsof.txt %dirname%\ >nul 2>&1
+	)
+	:: Get date so the installation is selected by default
+	FOR /F "tokens=* USEBACKQ" %%F IN (`echo %DATE%`) DO (
+		SET timedate=%%F
+	)
+	:: Format it correctly with the time
+	set timedate=!timedate:/=-!T%TIME%0Z
+	:: Add installation profile
+	cd %dirname%
+	CALL :addmodprofile
+	cd ..\.minecraft\
+	CALL :addmodprofile
+	endlocal
+) else (
+	echo Minecraft not detected. Skipping config edits.
 )
-:: Format it correctly with the time
-set timedate=%timedate:/=-%T%TIME%0Z
-:: Add installation profile
-cd %dirname%
-CALL :addmodprofile
-cd ..\.minecraft\
-CALL :addmodprofile
 echo.
 echo Picklemon successfully installed!
 pause
 EXIT /B %ERRORLEVEL%
 
 :addmodprofile
+if NOT EXIST launcher_profiles.json (
+	goto:eof
+)
 (
 echo {
 echo   "profiles" : {
